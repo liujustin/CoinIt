66f5da4225bc3b71ba960cb88062196642bb08b2
Justin Liu <jxl3908@rit.edu>
2018-01-30 17:16:53 -0500

    Changing text in header back to white

--- client/src/common/welcomeHeader.js
+++ client/src/common/welcomeHeader.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import '../css/App.css';
+import '../css/header.css';
 
 const WelcomeHeader = () => (
-    <div class="ui fixed inverted menu">
+    <div class="ui fixed menu">
         <div class="ui container">
             <a href="/" class="left header item">
-                <img class="logo" src="../assets/images/bitcoin.png" alt="crypto logo"/>
-                    CoinIt!
+                <img class="crypto-image" src="../assets/images/bitcoin.png" alt="crypto logo"/>
+                    <span class="logo-text">CoinIt!</span>
             </a>
             <div class="right menu">
-                <a href="/login" class="item">Login</a>
+                <a href="/login" class="item"><span class="menu-text">Login</span></a>
             </div>
             </div>
         </div>
