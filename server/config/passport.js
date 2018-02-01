1cacd07f8ed6fb7c8677d1d5c962abe008881301
Xin Liu <xhliu97@gmail.com>
2018-02-01 17:36:07 -0500

    in progress: started work on local login

--- server/config/passport.js
+++ server/config/passport.js
@@ -19,6 +19,7 @@ module.exports = function(passport) {
   // Local Signup
   // ============
   passport.use('local-signup', new LocalStrategy({
+    // by default, local strategy uses username and password, we will override with email
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback : true // allows us to pass back the entire request to the callback
@@ -58,14 +59,10 @@ module.exports = function(passport) {
     });
   }));
 
-  // =========================================================================
-  // LOCAL LOGIN =============================================================
-  // =========================================================================
-  // we are using named strategies since we have one for login and one for signup
-  // by default, if there was no name, it would just be called 'local'
-
+  // ============
+  // Local Login
+  // ============
   passport.use('local-login', new LocalStrategy({
-    // by default, local strategy uses username and password, we will override with email
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback : true // allows us to pass back the entire request to the callback
