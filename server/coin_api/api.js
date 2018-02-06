8a250abdc10d9078e70329de627e798a383eb6c2
Xin Liu <xhliu97@gmail.com>
2018-02-06 12:45:02 -0500

    added crypto logging back to server.js

--- server/coin_api/api.js
+++ server/coin_api/api.js
@@ -2,8 +2,19 @@ const axios = require('axios');
 const api_url = "https://api.coinmarketcap.com/v1/ticker/?limit=10";
 
 const apiConfig = (app) => {
+  axios.get(api_url).then(response => {
+      // loop through each for its name
+      for (var i = 0; i < response.data.length; i++) {
+          console.log('Name:' + response.data[i].name);
+          console.log('Symbol: ' + response.data[i].symbol);
+          console.log('Price in USD: ' + response.data[i].price_usd);
+          console.log('Rank: ' + response.data[i].rank);
+          console.log('===============');
+      }
+  })
+  
     app.get('/', (req, res) => {
-      res.send('Server is running.')
+      res.send('Server is running; Check terminal for latest crypto updates.')
     });
 };
 
