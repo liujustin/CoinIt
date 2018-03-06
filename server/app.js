6e302b6eb3d9ef4791b29d3d602e2c08d2f9d5fd
Xin Liu <xhliu97@gmail.com>
2018-03-06 15:04:44 -0500

    included passport, jwt, etc for user

--- server/app.js
+++ server/app.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const path = require('path');
-
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+// API Routes
 const configRoutes = require('./routes');
 
 // Express
 const app = express();
-
-// API Routes
 configRoutes(app);
 
-// Serve static files from React
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+// Bodyparser to get POST requests for API use
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Logger for application requests
+app.use(morgan('dev'));
+
+/**
+
+
+commented out for dev purposes
+
+// Serve static files from React build
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 // "catchall" for routes that do not match one of the routes
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build/', 'index.html'));
 });
+
+
+**/
 
 // Express server port
 const port = process.env.PORT || 8000;
